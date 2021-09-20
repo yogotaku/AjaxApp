f5ffe5e7d403df5297b00b7d952561416be58792
kaclar-ef <62864518+kaclar-ef@users.noreply.github.com>
2021-09-20 10:26:57 +0900

    test

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,12 +4,12 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../checked")
 require("../memo")
-require("../test")
+// require("../test")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- app/javascript/test.js
+++ app/javascript/test.js
@@ -1,57 +1,6 @@
-class Item {
-  constructor(item) {
-    this.id = item.id;
-    this.content = item.content;
-    this.isChecked = item.checked;
-    this.created_at = item.created_at;
-  }
+window.addEventListener('load', () => {
+  const hello = undefined || 100 || 0 || NaN || '' || 'Hello!'; const chao = "22222" && '33333' && 100 && [] && {} && 'Chao!';
 
-  buildHtml = () =>`
-                    <div class="post" data-id=${this.id}>
-                      <div class="post-date" >
-                        投稿日時:${this.created_at}
-                      </div>
-                      <div class="post-content">
-                        ${this.content}
-                      </div>
-                    </div> 
-                  `;
-
-  appendHtmlToList = (html) => {
-    const list = document.getElementById("list");
-    list.insertAdjacentHTML("afterend", html);
-  };
-}
-
-function memo() {
-  const submit = document.getElementById("submit");
-  submit.addEventListener("click", (e) => {
-    const formData = new FormData(document.getElementById("form"));
-    const XHR = new XMLHttpRequest();
-    XHR.open("POST", "/posts", true);
-    XHR.responseType = "json"
-    XHR.send(formData);
-    XHR.onload = () => {
-      if (XHR.status != 200) {
-        alert(`Error ${XHR.status}: ${XHR.statusText}`);
-      }
-      const item = new Item(XHR.response.post);
-      const html = item.buildHtml();
-      item.appendHtmlToList(html);
-      document.getElementById("content").value = "";
-    }
-
-    XHR.onerror = () => {
-      alert("Request failed");
-    }
-
-    e.preventDefault();
-  })
-}
-window.addEventListener("load", memo);
-
-//タイムゾーン
-
-
-
-
+  console.log(hello);
+  console.log(chao);
+})
